e20fbe955a409e465851fd0a4e753e618e020cc4
Daeyeon Joo <daeyeonjoo@p-ful.com>
2019-10-21 10:21:13 +0900

    -- docker-compose 용 파일 추가

--- run_cluster.sh
+++ run_cluster.sh
@@ -1,17 +1,3 @@
 #!/bin/bash
 
-docker network create spark_network
-
-docker run -d \
-           --name spark-master \
-           --hostname spark-master \
-           --network spark_network \
-           -p 7077:7077 -p 8080:8080 \
-           daeyeonjoo/spark_master:2.4.4
-
-docker run -d \
-           --name spark-worker1 \
-           --hostname spark-worker1 \
-           --network spark_network \
-           -e MASTER_URL=spark://spark-master:7077 \
-           daeyeonjoo/spark_slave:2.4.4
+docker-compose up
--- run_sample.sh
+++ run_sample.sh
@@ -4,3 +4,10 @@ docker run --rm \
        -e MASTER_URL=spark://spark-master:7077 \
        --network spark_network \
        daeyeonjoo/spark_sample_app:2.4.4
+
+
+
+docker run --rm \
+       -e MASTER_URL=spark://localhost:31626 \
+       --network spark_network \
+       daeyeonjoo/spark_sample_app:2.4.4
